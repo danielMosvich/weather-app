5d14e163507b32f1cd9b132a6ed338af9eb6ff7d
danielMosvich <mrdaniraymay@gmail.com>
2023-03-07 09:08:09 -0500

    solution

--- src/services/+weather.js
+++ src/services/+weather.js
@@ -1,5 +1,5 @@
 export async function getWeatherFrom(country) {
-    const response = await fetch(`https://weather-app-eight-ashy-34.vercel.app/api/${country}`)
+    const response = await fetch(`https://weather-app-daniraygod.vercel.app/api/${country}`)
     const data = await response.json()
     return data
 }
